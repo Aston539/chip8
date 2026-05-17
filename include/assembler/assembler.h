554dec1cafd6e979e8150cc6308af5f4cfa98a75
MxCopyPage <snowdenaston@gmail.com>
2026-05-17 11:39:23 +0100

    removed lexer responsibilities from assembler

--- include/assembler/assembler.h
+++ include/assembler/assembler.h
@@ -3,9 +3,11 @@
 
 #include "defines.h"
 
+#include <isa/instruction.h>
+
 UINT16
-Chip8AssembleStatement(
-    _In_ LPCSTR Statement
+Chip8AssembleInstruction(
+    _In_ CONST CHIP8_INSTRUCTION CONST* Instruction
 );
 
 #endif // _CHIP8_ASSEMBLER_H_
--- source/assembler/assembler.cpp
+++ source/assembler/assembler.cpp
@@ -2,41 +2,10 @@
 #include <encoder/encoder.h>
 #include <disassembler/disassembler.h>
 
-#define CHIP8_ASSEMBLER_TOKEN_TYPE_UNKNOWN  0
-#define CHIP8_ASSEMBLER_TOKEN_TYPE_MNEMONIC 1
-#define CHIP8_ASSEMBLER_TOKEN_TYPE_REGSTER  2
-#define CHIP8_ASSEMBLER_TOKEN_TYPE_INTEGRAL 3
-
-typedef struct _CHIP8_TOKEN
-{
-    BYTE Type;
-
-    LPCSTR Lexeme;
-
-    ULONG Line;
-    ULONG Column;
-
-} CHIP8_TOKEN, *PCHIP8_TOKEN;
-
 UINT16
-Chip8AssembleStatement(
-    _In_ LPCSTR Statement
+Chip8AssembleInstruction(
+    _In_ CONST CHIP8_INSTRUCTION CONST* Instruction
 )
 {
-    if ( stricmp( Statement, "CLS" ) == 0 )
-    {
-        return Chip8EncodeInstruction( CHIP8_OPCODE_KEY_SPECIAL_CLS, NULL, NULL, NULL, NULL );
-    }
-    else if ( stricmp( Statement, "RET" ) == 0 )
-    {
-        return Chip8EncodeInstruction( CHIP8_OPCODE_KEY_SPECIAL_RET, NULL, NULL, NULL, NULL );
-    }
-    else if ( stricmp( Statement, "JMP" ) == 0 )
-    {
 
-    }
-    else if ( stricmp( Statement, "CALL" ) == 0 )
-    {
-        return Chip8EncodeInstruction( CHIP8_OPCODE_KEY_CALL, NULL, NULL, NULL, NULL );
-    }
 }
