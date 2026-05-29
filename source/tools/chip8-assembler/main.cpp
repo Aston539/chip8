75843798df8aff8142b86f0fbd9abc9addb50944
MxCopyPage <snowdenaston@gmail.com>
2026-05-29 23:36:51 +0100

    using command line arguments for cli assembler

--- source/tools/chip8-assembler/main.cpp
+++ source/tools/chip8-assembler/main.cpp
@@ -41,121 +41,46 @@ IsArgumentPath(
 
 int main( int ArgumentCount, char** Arguments )
 {
-    LPCSTR TestStatement = 
-R"(CLS
-MOV R10, 032
-MOV R11, 01B
-MOV INDEX, 609
-DRAW R10, R11, 004
-MOV R10, 03A
-MOV INDEX, 60D
-DRAW R10, R11, 004
-MOV R13, 000
-MOV R14, 000
-MOV INDEX, 5F7
-CALL 210
-MOV R10, 016
-MOV R11, 000
-MOV R1, 00F
-MOV R13, 001
-CALL 280
-MOV R3, 00F
-MOV R15, 014
-OR R3, R15
-MOV R15, 000
-MOV R2, 032
-OR R2, R1
-MOV R14, R15
-MOV R12, 03F
-CALL 290
-MOV R2, R14
-MOV R12, 000
-CALL 290
-MOV R2, R3
-MOV R12, 01F
-CALL 290
-ADD R10, 005
-MOV R13, 002
-CALL 280
-MOV R3, 00F
-MOV R15, 014
-AND R3, R15
-MOV R15, 000
-MOV R2, 032
-AND R2, R1
-MOV R14, R15
-MOV R12, 002
-CALL 290
-MOV R2, R14
-MOV R12, 000
-CALL 290
-MOV R2, R3
-MOV R12, 004
-CALL 290
-ADD R11, 005
-MOV R10, 000
-MOV R13, 003
-CALL 280
-MOV R3, 00F
-MOV R15, 014
-XOR R3, R15
-MOV R15, 000
-MOV R2, 032
-XOR R2, R1
-MOV R14, R15
-MOV R12, 03D
-CALL 290
-MOV R2, R14
-MOV R12, 000
-CALL 290
-MOV R2, R3
-MOV R12, 01B
-CALL 290
-ADD R10, 005
-MOV R13, 004
-CALL 280
-MOV R15, 014
-ADD R15, R1
-MOV R4, R15
-MOV R3, 00F
-MOV R15, 014
-ADD R3, R15
-MOV R15, 0AA
-MOV R2, 032
-ADD R2, R1
-MOV R14, R15
-MOV R12, 041
-CALL 290
-MOV R2, R14
-MOV R12, 000
-CALL 290
-MOV R2, R3
-MOV R12, 023
-CALL 290
-MOV R2, R4
-MOV R12, 000
-CALL 290
-ADD R10, 001
-MOV R13, 005
-CALL 280
-MOV R15, 014
-SUB R15, R1
-MOV R4, R15
-MOV R3, 014
-MOV R15, 00F
-SUB R3, R15
-MOV R5, 00A
-MOV R15, 00A
-SUB R5, R15
-MOV R5, R15
-MOV R15, 0AA
-MOV R2, 032
-SUB R2, R1
-SE R5, 001
-)";
+    if ( ArgumentCount < 2 )
+    {
+        DisplayUsage( );
+
+        return 1;
+    }
+
+    std::string ProgramCode = { };
+    if ( IsArgumentPath( Arguments[ 1 ] ) )
+    {
+        std::ifstream File( Arguments[ 1 ], std::ios::in | std::ios::binary );
+        if ( File.is_open( ) == FALSE )
+        {
+            return 2;
+        }
+
+        File.seekg( NULL, std::ios::end );
+        std::streamsize FileSize = File.tellg( );
+
+        if ( FileSize <= NULL )
+        {
+            return 3;
+        }
+
+        ProgramCode.resize( FileSize, '\0' );
+
+        File.read( ProgramCode.data( ), FileSize );
+    }
+    else
+    {
+        for ( ULONG ArgumentIndex = 1;
+                    ArgumentIndex < ArgumentCount;
+                    ArgumentIndex++ )
+        {
+            ProgramCode.append( Arguments[ ArgumentIndex ] );
+        }
+    }
 
     CHIP8_LEXER Lexer = { };
-    Chip8LexerInitialize( &Lexer, TestStatement, strlen( TestStatement ) );
+    Chip8LexerInitialize( &Lexer, ProgramCode.c_str( ), ProgramCode.size( ) );
 
     CHIP8_LEXER_TOKEN Token = { };
     while ( Chip8LexerAdvanceToken( &Lexer, &Token ) )
@@ -179,27 +104,5 @@ SE R5, 001
         strncpy( TokenText, Token.TextStart, CopySize );
 
         printf( "%s\n", TokenText );
-    }
-
-    if ( ArgumentCount < 2 )
-    {
-        DisplayUsage( );
-
-        return 1;
-    }
-
-    if ( IsArgumentPath( Arguments[ 1 ] ) )
-    {
-
-    }
-    else
-    {
-        for ( ULONG StatementIndex = 1; 
-                    StatementIndex < ArgumentCount; 
-                    StatementIndex++ )
-        {
-            LPCSTR Statement = Arguments[ StatementIndex ];
-            Chip8LexToken( Statement, NULL, NULL );
-        }
     }
 }
